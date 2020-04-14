0bb6f6b95fdcbdc9d6a5847d74caa9d41b9a8c91
Jeff <jeff.uca@gmail.com>
2020-04-14 09:17:04 +0200

    :recycle: ts -> ts and add basic components of all  views

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+export default (): JSX.Element => ) <View><Text>Home</Text></View>
---
+++ src/views/Login/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+
+export default (): JSX.Element =>  <View><Text>Login</Text></View>
