01f76114c6bf872f23a4ab8942caa9df94510ca2
Jeff <jeff.uca@gmail.com>
2020-04-14 11:34:38 +0200

    :art: format code of routes index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack'
 import {NavigationContainer} from '@react-navigation/native';
-
-
 import {
   LOADING_PAGE,
   LOGIN,
@@ -10,17 +8,14 @@ import {
   RESULTS,
   PROFILE,
 } from '../consts';
-
 import Login  from '../views/Login'
 import Home from '../views/Home'
 import LoadingPage from '../views/LoadingPage'
 import Results from '../views/Results'
 import Profile from  '../views/Profile'
 
-
 export const RootStack = ():JSX.Element => {
   const Stack = createStackNavigator()
-
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -30,28 +25,23 @@ export const RootStack = ():JSX.Element => {
           name={LOADING_PAGE}
           component={LoadingPage}
         />
-
         <Stack.Screen
           name={LOGIN}
           component={Login}
         />
-
         <Stack.Screen
           name={HOME}
           component={Home}
         />
-
         <Stack.Screen
           name={RESULTS}
           component={Results}
         />
-
         <Stack.Screen
           name={PROFILE}
           component={Profile}
         />
       </Stack.Navigator>
-
     </NavigationContainer>
   )
 };
