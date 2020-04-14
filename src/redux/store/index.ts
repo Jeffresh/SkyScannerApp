9df7db98d4a200fee03151b57effdfd0b44eed4d
Jeff <jeff.uca@gmail.com>
2020-04-14 11:08:31 +0200

    :sparkles: add sample reducer

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import reducers from '../reducers'
 
 export default () => {
   const sagaMiddleware = createSagaMiddleware()
