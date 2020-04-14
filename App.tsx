d57e73c18826dd9c85ea176828eb91ab733559f1
Jeff <jeff.uca@gmail.com>
2020-04-14 09:32:14 +0200

    :sparkles: add navigator to main app
    delete styles and other  unused content too

--- App.tsx
+++ App.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import AppContainer from './src'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world, react native app!</Text>
-    </View>
-  );
+  return <AppContainer/>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
