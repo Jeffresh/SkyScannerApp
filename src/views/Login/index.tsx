635b83bcaacf1c1011d0d7d48892bfc95924fe5b
Jeff <jeff.uca@gmail.com>
2020-04-14 13:05:47 +0200

    :sparkles: add relative text and google button to Login screen

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Container, Content, Text } from 'native-base'
-
+import { Container, Content, Text, Grid, Button } from 'native-base'
+import styles from './style';
 const Login = ():JSX.Element => {
   return(
     <Container>
-      <Content>
-        <Text>Login</Text>
+      <Content contentContainerStyle={styles.content}>
+        <Grid style={styles.grid}>
+          <Text>Welcome!</Text>
+          <Text>Sign in to continue</Text>
+          <Button light>
+            <Text>
+              Google
+            </Text>
+          </Button>
+        </Grid>
       </Content>
     </Container>
 
