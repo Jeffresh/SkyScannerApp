6df84eee4af2b36900ea5ef0ec5fdab2e5127ae4
Jeff <jeff.uca@gmail.com>
2020-04-14 11:30:19 +0200

    :sparkles: add store to src index

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
 import { RootStack as Routes } from './routes';
+import { Provider } from 'react-redux'
+import Store from './redux/store'
 
-export default (): JSX.Element => <Routes />
+const store = Store()
+
+export default (): JSX.Element => (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+)
