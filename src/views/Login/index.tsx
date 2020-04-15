1cde79e0d1da0ec27b29c7610ead8881c69debf2
Jeff <jeff.uca@gmail.com>
2020-04-15 17:27:20 +0200

    :sparkles: add button and function to login through google

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import { Image } from 'react-native'
 import { Container, Content, Text, Grid, Button } from 'native-base'
+import * as Google from 'expo-google-app-auth'
 import styles from './style';
+import { CLIENT_IDS } from '../../../clientIds'
 const GOOGLE_IMAGE = require('../../../assets/google-icon.png')
+
 const Login = ():JSX.Element => {
+
+  const handleLoginPress = async () => {
+    const result = await Google.logInAsync({
+     iosClientId: CLIENT_IDS.iosClientId,
+      androidClientId: CLIENT_IDS.androidClientId,
+      iosStandaloneAppClientId: CLIENT_IDS.iosStandaloneAppClientId,
+      androidStandaloneAppClientId: CLIENT_IDS.androidStandaloneAppClientId,
+    })
+    console.log(result)
+  }
+
   return(
     <Container>
       <Content contentContainerStyle={styles.content}>
         <Grid style={styles.grid}>
           <Text style={styles.title}>Welcome!</Text>
           <Text style={styles.subtitle}>Sign in to continue</Text>
-          <Button light style={styles.googleBtn}>
+          <Button light style={styles.googleBtn} onPress={handleLoginPress}>
             <Text>
               Google
             </Text>
-            <Image source={GOOGLE_IMAGE} style={styles.googleIcon}/>
+            <Image source={GOOGLE_IMAGE} style={styles.googleIcon} />
           </Button>
         </Grid>
       </Content>
