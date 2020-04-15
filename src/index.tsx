5497e0db9226df49759e3436f75af5c7b6e24538
Jeff <jeff.uca@gmail.com>
2020-04-15 14:37:18 +0200

    :bug: fix robotofonts error at dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { RootStack as Routes } from './routes';
 import { Provider } from 'react-redux'
 import Store from './redux/store'
+import * as Font from 'expo-font'
+import {Spinner} from 'native-base';
+import { ROBOTO_FONT } from './consts';
 
 const store = Store()
 
-export default (): JSX.Element => (
-  <Provider store={store}>
-    <Routes />
-  </Provider>
-)
+export default (): JSX.Element => {
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      'Roboto': ROBOTO_FONT.roboto,
+      'Roboto_medium': ROBOTO_FONT.robotoMedium,
+    })
+    setFontsLoaded(true)
+  }
+  useEffect( () => {
+    if(!fontsLoaded){
+      loadFonts()
+    }
+  }, [fontsLoaded])
+
+
+  if(!fontsLoaded) {
+    return <Spinner color="red"/>
+  }
+  return (
+    <Provider store={store}>
+      <Routes/>
+    </Provider>
+  )
+}
