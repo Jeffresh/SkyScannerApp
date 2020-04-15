d63a7ed8a4a1c8711740324e27ca606d3ae02009
Jeff <jeff.uca@gmail.com>
2020-04-15 14:36:33 +0200

    :sparkles: add roboto fonts

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -3,3 +3,8 @@ export const LOGIN: string = 'Login'
 export const HOME: string = 'Home'
 export const RESULTS: string = 'Results'
 export const PROFILE: string = 'Profile'
+
+export const ROBOTO_FONT = {
+  roboto: require('../../node_modules/native-base/Fonts/Roboto.ttf'),
+  robotoMedium: require('../../node_modules/native-base/Fonts/Roboto_medium.ttf'),
+}
