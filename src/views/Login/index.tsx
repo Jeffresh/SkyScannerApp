582a5405e64f394ac73e175163b725c5051a8bad
Jeff <jeff.uca@gmail.com>
2020-04-15 18:12:52 +0200

    :sparkles: add function to add item into storage
    add user info

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -3,21 +3,29 @@ import { Image } from 'react-native'
 import { Container, Content, Text, Grid, Button } from 'native-base'
 import * as Google from 'expo-google-app-auth'
 import styles from './style';
+import { saveItem } from '../../utils/storage';
+
 import { CLIENT_IDS } from '../../../clientIds'
 const GOOGLE_IMAGE = require('../../../assets/google-icon.png')
 
 const Login = ():JSX.Element => {
 
   const handleLoginPress = async () => {
-    const result = await Google.logInAsync({
-     iosClientId: CLIENT_IDS.iosClientId,
-      androidClientId: CLIENT_IDS.androidClientId,
-      iosStandaloneAppClientId: CLIENT_IDS.iosStandaloneAppClientId,
-      androidStandaloneAppClientId: CLIENT_IDS.androidStandaloneAppClientId,
-    })
-    console.log(result)
+    try{
+      const result = await Google.logInAsync({
+        iosClientId: CLIENT_IDS.iosClientId,
+        androidClientId: CLIENT_IDS.androidClientId,
+        iosStandaloneAppClientId: CLIENT_IDS.iosStandaloneAppClientId,
+        androidStandaloneAppClientId: CLIENT_IDS.androidStandaloneAppClientId,
+      })
+      if(result.type === 'success') {
+        const userResult = await saveItem('user-info', JSON.stringify(result.user))
+        console.log(userResult)
+      }
+    } catch (e) {
+      console.log(e)
+    }
   }
-
   return(
     <Container>
       <Content contentContainerStyle={styles.content}>
