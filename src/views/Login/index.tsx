3d408ba1e4747b47c85a6e2a48c4844e67122a16
Jeff <jeff.uca@gmail.com>
2020-04-16 10:14:50 +0200

    :sparkles: add alerts and try catch

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -5,26 +5,41 @@ import * as Google from 'expo-google-app-auth'
 import styles from './style';
 import { saveItem } from '../../utils/storage';
 
+import {ACCESS_TOKEN, USER_INFO, GOOGLE_SUCCESS_MESSAGE} from '../../consts';
 import { CLIENT_IDS } from '../../../clientIds'
 const GOOGLE_IMAGE = require('../../../assets/google-icon.png')
 
 const Login = ():JSX.Element => {
 
   const handleLoginPress = async () => {
-    try{
+    try {
       const result = await Google.logInAsync({
-        iosClientId: CLIENT_IDS.iosClientId,
         androidClientId: CLIENT_IDS.androidClientId,
-        iosStandaloneAppClientId: CLIENT_IDS.iosStandaloneAppClientId,
         androidStandaloneAppClientId: CLIENT_IDS.androidStandaloneAppClientId,
       })
-      if(result.type === 'success') {
-        const userResult = await saveItem('user-info', JSON.stringify(result.user))
-        console.log(userResult)
+
+      if (result.type === GOOGLE_SUCCESS_MESSAGE) {
+        console.log('Success!')
+        console.log(result.user)
+        try {
+          const userResult = await saveItem(USER_INFO, JSON.stringify(result.user))
+          if(result.accessToken !== null) {
+            const tokenResult = await saveItem(ACCESS_TOKEN, result.accessToken)
+          }
+          else {
+            alert('error: Null access token')
+          }
+        } catch (e) {
+          alert(e)
+        }
+      } else {
+        console.log('Not success')
       }
-    } catch (e) {
-      console.log(e)
     }
+    catch (e) {
+      alert(e)
+    }
+
   }
   return(
     <Container>
@@ -33,15 +48,12 @@ const Login = ():JSX.Element => {
           <Text style={styles.title}>Welcome!</Text>
           <Text style={styles.subtitle}>Sign in to continue</Text>
           <Button light style={styles.googleBtn} onPress={handleLoginPress}>
-            <Text>
-              Google
-            </Text>
+            <Text> Google </Text>
             <Image source={GOOGLE_IMAGE} style={styles.googleIcon} />
           </Button>
         </Grid>
       </Content>
     </Container>
-
   )
 }
 
