89db31ac402951630fd02a73415b9592ef593f0e
Jeff <jeff.uca@gmail.com>
2020-04-16 12:32:24 +0200

    :sparkles: add to Home screen header with photo of the logged user

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,4 +1,26 @@
-import React from 'react'
-import {View, Text} from 'react-native';
+import React, {useState,useEffect} from 'react'
+import {Container, Content, Text} from 'native-base';
+import { Header } from '../../components/Header'
 
-export default (): JSX.Element =>  <View><Text>Home</Text></View>
+import { getItem } from '../../utils/storage';
+import { USER_INFO } from '../../consts';
+
+export const Home = (): JSX.Element => {
+  const [userInfo, setUserInfo] = useState(null)
+
+  const loadUserInfo = async () => {
+    let userInfo = await getItem(USER_INFO)
+    setUserInfo(userInfo)
+  }
+  useEffect(() => {
+    if(!userInfo){
+      loadUserInfo()
+    }
+  },[userInfo])
+
+  return (
+    <Container>
+      <Header imageUri={{uri: userInfo && userInfo.photoUrl}}/>
+    </Container>
+  )
+}
