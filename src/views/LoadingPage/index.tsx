4accace5e53d26acddf472d0cfa3e31a675f59b4
Jeff <jeff.uca@gmail.com>
2020-04-16 11:57:50 +0200

    :sparkles: home if logged previously if not login screen
    saving personal token

--- src/views/LoadingPage/index.tsx
+++ src/views/LoadingPage/index.tsx
@@ -1,11 +1,20 @@
 import React, {useEffect} from 'react'
 import { View, Text, Spinner, Container, Content, Grid } from 'native-base'
-import { LOGIN } from '../../consts'
+import { LOGIN, HOME, ACCESS_TOKEN } from '../../consts'
+import { getItem } from '../../utils/storage'
 import styles from './style'
 
 export default ({navigation}:any): JSX.Element => {
+  const redirect = async () => {
+    const token = await getItem(ACCESS_TOKEN)
+    let route = LOGIN
+    if(token){
+      route = HOME
+    }
+    navigation.navigate(route)
+  }
   useEffect(()=> {
-    navigation.navigate(LOGIN)
+    redirect()
   })
   return(
     <Container>
