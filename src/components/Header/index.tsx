69ba58c4a7d89ad2b28805137d1cded9f3d93710
Jeff <jeff.uca@gmail.com>
2020-04-17 08:33:36 +0200

    :lipstick: add colors to header/android notification bar
    using color constants

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Header as NBHeader, Left, Body, Right, Thumbnail} from 'native-base'
-export const Header = ({imageUri}: any): JSX.Element => {
+import { PRIMARY_DARK } from '../../consts';
+import styles from './style';
 
+export const Header = ({imageUri}: any): JSX.Element => {
   return (
-    <NBHeader>
+    <NBHeader androidStatusBarColor={PRIMARY_DARK} style={styles.header}>
       <Left/>
       <Body/>
       <Right>
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,5 +1,8 @@
 import { StyleSheet } from 'react-native';
+import { PRIMARY } from '../../consts';
 
 export default StyleSheet.create({
-
+  header: {
+    backgroundColor: PRIMARY
+  }
 })
