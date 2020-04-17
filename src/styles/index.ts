4655e2ad99ed79af119ab11e8ab8efba9f08f0a4
Jeff <jeff.uca@gmail.com>
2020-04-17 11:12:29 +0200

    :lipstick:  add generic styles

---
+++ src/styles/index.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  centeredContent: {
+    flex: 1,
+  },
+  centeredGrid: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
