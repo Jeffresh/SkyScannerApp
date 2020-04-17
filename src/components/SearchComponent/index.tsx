f66491d8a6456291698602c6b417785177c8aa69
Jeff <jeff.uca@gmail.com>
2020-04-17 10:11:51 +0200

    :sparkles: picker base to choose adult's/adult's number

--- src/components/SearchComponent/index.tsx
+++ src/components/SearchComponent/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState , useEffect } from 'react';
-import {Form, Input, Item, Label, Icon, DatePicker} from 'native-base';
+import {Form, Input, Item, Label, Icon, DatePicker, Picker} from 'native-base';
 
 export const SearchComponent = (props : any) => {
   return (
@@ -21,6 +21,31 @@ export const SearchComponent = (props : any) => {
         <Icon ios="ios-calendar" android='md-calendar'/>
         <DatePicker placeHolderText="Arrival date (optional)"
         />
+      </Item>
+      <Item>
+        <Picker placeholder="Select your SIM" mode='dropdown'>
+          <Picker.Item label="Adult's number" key="0"/>
+          <Picker.Item label="2" key="2"/>
+          <Picker.Item label="3" key="3"/>
+          <Picker.Item label="4" key="4"/>
+          <Picker.Item label="5" key="5"/>
+          <Picker.Item label="6" key="6"/>
+          <Picker.Item label="7" key="7"/>
+        </Picker>
+        <Icon name='person'/>
+      </Item>
+      <Item>
+        <Picker placeholder="Select your SIM" mode='dropdown'>
+          <Picker.Item label="Children's number" key="0"/>
+          <Picker.Item label="1" key="1"/>
+          <Picker.Item label="2" key="2"/>
+          <Picker.Item label="3" key="3"/>
+          <Picker.Item label="4" key="4"/>
+          <Picker.Item label="5" key="5"/>
+          <Picker.Item label="6" key="6"/>
+          <Picker.Item label="7" key="7"/>
+        </Picker>
+        <Icon name='person'/>
       </Item>
     </Form>
   )
