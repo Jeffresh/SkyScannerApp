ca1f5615124d692c918cd95377254b9d2af32f04
Jeff <jeff.uca@gmail.com>
2020-04-17 09:33:52 +0200

    :lipstick: add icons to origin and destination form inputs

--- src/components/SearchComponent/index.tsx
+++ src/components/SearchComponent/index.tsx
@@ -1,13 +1,15 @@
 import React, { useState , useEffect } from 'react';
-import { Form, Input, Item, Label} from 'native-base';
+import { Form, Input, Item, Label, Icon} from 'native-base';
 
 export const SearchComponent = (props : any) => {
   return (
     <Form>
       <Item>
+        <Icon name='ios-home'/>
         <Input placeholder="Origin"/>
       </Item>
       <Item>
+        <Icon name='ios-airplane'/>
         <Input placeholder="Destination"/>
       </Item>
     </Form>
