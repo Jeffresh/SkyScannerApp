98d381578c53194c8f14df178b1521c917325ad2
Jeff <jeff.uca@gmail.com>
2020-04-17 08:34:19 +0200

    :art: add comments and format to organize constant file

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,19 +1,23 @@
+// Routes
 export const LOADING_PAGE: string = 'LoadingPage'
 export const LOGIN: string = 'Login'
 export const HOME: string = 'Home'
 export const RESULTS: string = 'Results'
 export const PROFILE: string = 'Profile'
 
+//Fonts
 export const ROBOTO_FONT = {
   roboto: require('../../node_modules/native-base/Fonts/Roboto.ttf'),
   robotoMedium: require('../../node_modules/native-base/Fonts/Roboto_medium.ttf'),
 }
-
+//Info
 export const ACCESS_TOKEN = '@access-token'
 export const USER_INFO = '@user-info'
 
+//Messages
 export const GOOGLE_SUCCESS_MESSAGE = 'success'
 
+//Colors
 export const PRIMARY = '#512da8'
 export const PRIMARY_DARK = '#311b92'
 export  const SECONDARY = '#f5b72c'
