9d792bba8553f521e256bfc8bf31c20b326f7972
Jeff <jeff.uca@gmail.com>
2020-04-17 10:56:46 +0200

    :sparkles: add  useState on SearchComponent: adult/children number
    get/set adults/children number on picler

--- src/components/SearchComponent/index.tsx
+++ src/components/SearchComponent/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Form, Input, Item, Label, Icon, DatePicker, Picker} from 'native-base';
 
 export const SearchComponent = (props : any) => {
@@ -6,10 +6,17 @@ export const SearchComponent = (props : any) => {
   const [destinationPlace, setDestinationPlace] = useState('')
   const [outBoundDate, setOutBoundDate] = useState({} as Date)
   const [inBoundDate, setInBoundDate] = useState({} as Date)
+  const [adultsNumber, setAdultsNumber] = useState('0')
+  const [childrenNumber, setChildrenNumber] = useState('0')
+
   const handleOriginPlaceChange = (origin:string) => setOriginPlace(origin)
   const handleDestinationPlaceChange = (destination:string) => setDestinationPlace(destination)
+
   const handleInBoundDateChange = (inboundDate: Date) => setInBoundDate(inboundDate)
   const handleOutBoundDateChange = (outboundDate: Date) => setOutBoundDate(outboundDate)
+
+  const handleAdultsNumberChange = (adultsNumber: string) => setAdultsNumber(adultsNumber)
+  const handleChildrenNumberChange = (childrenNumber: string) => setChildrenNumber(childrenNumber)
 
   return (
     <Form>
@@ -30,28 +37,39 @@ export const SearchComponent = (props : any) => {
         <DatePicker placeHolderText="Arrival date (optional)" onDateChange={handleOutBoundDateChange}/>
       </Item>
       <Item>
-        <Picker placeholder="Select your SIM" mode='dropdown'>
+        <Picker
+          placeholder="Select your SIM"
+          mode='dropdown'
+          selectedValue={adultsNumber}
+          onValueChange={handleAdultsNumberChange}
+        >
+
           <Picker.Item label="Adult's number" key="0"/>
-          <Picker.Item label="1" key="1"/>
-          <Picker.Item label="2" key="2"/>
-          <Picker.Item label="3" key="3"/>
-          <Picker.Item label="4" key="4"/>
-          <Picker.Item label="5" key="5"/>
-          <Picker.Item label="6" key="6"/>
-          <Picker.Item label="7" key="7"/>
+          <Picker.Item label="1" value="1"/>
+          <Picker.Item label="2" value="2"/>
+          <Picker.Item label="3" value="3"/>
+          <Picker.Item label="4" value="4"/>
+          <Picker.Item label="5" value="5"/>
+          <Picker.Item label="6" value="6"/>
+          <Picker.Item label="7" value="7"/>
         </Picker>
         <Icon name='person'/>
       </Item>
       <Item>
-        <Picker placeholder="Select your SIM" mode='dropdown'>
-          <Picker.Item label="Children's number" key="0"/>
-          <Picker.Item label="1" key="1"/>
-          <Picker.Item label="2" key="2"/>
-          <Picker.Item label="3" key="3"/>
-          <Picker.Item label="4" key="4"/>
-          <Picker.Item label="5" key="5"/>
-          <Picker.Item label="6" key="6"/>
-          <Picker.Item label="7" key="7"/>
+        <Picker
+          placeholder="Select your SIM"
+          mode='dropdown'
+          selectedValue={childrenNumber}
+          onValueChange={handleChildrenNumberChange}
+        >
+          <Picker.Item label="Children's number" value="0"/>
+          <Picker.Item label="1" value="1"/>
+          <Picker.Item label="2" value="2"/>
+          <Picker.Item label="3" value="3"/>
+          <Picker.Item label="4" value="4"/>
+          <Picker.Item label="5" value="5"/>
+          <Picker.Item label="6" value="6"/>
+          <Picker.Item label="7" value="7"/>
         </Picker>
         <Icon name='person'/>
       </Item>
