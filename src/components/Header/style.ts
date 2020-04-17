db118b0dbe33a0f6d413f4f956928f6efb4e6f18
Jeff <jeff.uca@gmail.com>
2020-04-17 12:12:06 +0200

    :bug: fix margin top header
    space between notification bar

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,8 +1,10 @@
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants'
 import { PRIMARY } from '../../consts';
 
 export default StyleSheet.create({
   header: {
-    backgroundColor: PRIMARY
+    backgroundColor: PRIMARY,
+    // marginTop: Constants.platform?.android ? Constants.statusBarHeight:0 | this dont work on my project
   }
 })
