7998652643a483abd919992e039fc47b667f11ad
Jeff <jeff.uca@gmail.com>
2020-04-20 12:08:06 +0200

    :lipstick: basic styles to show list under input

--- src/components/FixedList/style.ts
+++ src/components/FixedList/style.ts
@@ -1,5 +1,12 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+const { width } = Dimensions.get("window")
 
 export default StyleSheet.create({
+  container: {
+    position: 'absolute',
+    height: 250,
+    width,
+    zIndex: 1
+  }
 
 })
