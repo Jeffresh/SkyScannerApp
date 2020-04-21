b21b937b9c51e90dd629f17e084eab59dcf222ce
Jeff <jeff.uca@gmail.com>
2020-04-21 09:14:07 +0200

    :sparkles: add logic to get parameters in results screen
    using destructuring

--- src/views/Results/index.tsx
+++ src/views/Results/index.tsx
@@ -1,4 +1,24 @@
 import React from 'react'
 import {View, Text} from 'react-native';
 
-export default (): JSX.Element =>  <View><Text>Results</Text></View>
+export default (navigation : any): JSX.Element =>
+{
+  const {
+    route: {
+      params: {
+        adultsNumber,
+        childrenNumber,
+        originPlace,
+        destinationPlace,
+        inBoundDate,
+        outBoundDate,
+      }
+    }
+  } = navigation
+
+  return(
+    <View>
+      <Text>Results</Text>
+    </View>
+  )
+}
