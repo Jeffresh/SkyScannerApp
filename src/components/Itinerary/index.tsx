6a40d13357b899c22d0501b1e84bc66305cd5a10
Jeff <jeff.uca@gmail.com>
2020-04-21 11:24:19 +0200

    :sparkles: add basic component Itinerary
    styles file and added to results

---
+++ src/components/Itinerary/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card, CardItem, Text } from 'native-base';
+
+export const Itinerary = () => {
+  <Card>
+    <CardItem header>
+      <Text>Itinerary #</Text>
+    </CardItem>
+  </Card>
+
+}
--- src/views/Results/index.tsx
+++ src/views/Results/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import {useDispatch, useSelector } from 'react-redux';
 import {View, Text} from 'react-native';
 import {getItineraries} from '../../redux/actions/itineraries';
+import { Itinerary } from '../../components/Itinerary'
 
 export default (navigation : any): JSX.Element =>
 {
