9b243a0b8d227a3014296d106f7d304ded34ad8f
Jeff <jeff.uca@gmail.com>
2020-04-21 10:03:31 +0200

    :sparkles: add logic to pass itineraries information to saga

--- src/views/Results/index.tsx
+++ src/views/Results/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {useDispatch, useSelector } from 'react-redux';
 import {View, Text} from 'react-native';
 import {getItineraries} from '../../redux/actions/itineraries';
 
 export default (navigation : any): JSX.Element =>
 {
+  const[hasFetched, setHasFetched] = useState(false)
+  const dispatch = useDispatch()
+
   const {
     route: {
       params: {
@@ -17,6 +21,19 @@ export default (navigation : any): JSX.Element =>
     }
   } = navigation
 
+  useEffect(() => {
+    if(!hasFetched) {
+      dispatch(getItineraries({
+        adultsNumber,
+        childrenNumber,
+        originPlace,
+        destinationPlace,
+        inBoundDate,
+        outBoundDate
+      }))
+      setHasFetched(true)
+    }
+  })
   return(
     <View>
       <Text>Results</Text>
