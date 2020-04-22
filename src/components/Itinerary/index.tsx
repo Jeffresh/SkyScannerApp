2e20c6469a56d59140492b47605f53d69a4f448c
Jeff <jeff.uca@gmail.com>
2020-04-22 09:59:30 +0200

    :sparkles: add link to search in google the flight pressed

--- src/components/Itinerary/index.tsx
+++ src/components/Itinerary/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Card, CardItem, Text } from 'native-base';
+import { Linking } from 'react-native'
 import styles from './style';
 
 export const Itinerary = ({Itinerary, flightNumber}:any,): JSX.Element => {
 
-  console.log(Itinerary)
+  const handleItineraryOptionPress = (name: string) => {
+    const url = `https://google.com/search?q=${name}`
+    Linking.openURL(url)
+  }
 
   return (
+
     <Card>
-      <CardItem style={styles.cardItem}>
+      <CardItem style={styles.cardItem} button onPress={() => handleItineraryOptionPress(Itinerary.Name)}>
         <Text style={styles.placeTitle}>Flight #{flightNumber} </Text>
         <Text style={styles.label}>Id: {Itinerary.CarrierId}</Text>
         <Text style={styles.label}>Name: {Itinerary.Name}</Text>
