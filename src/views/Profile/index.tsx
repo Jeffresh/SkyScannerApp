60a38924dbaa26e1fa5034ea29500f31950586b8
Jeff <jeff.uca@gmail.com>
2020-04-22 14:21:27 +0200

    :sparkles: add logic to get user information from profile

--- src/views/Profile/index.tsx
+++ src/views/Profile/index.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import {Container, Content, View, Text} from "native-base"
+import React, {SetStateAction, useEffect, useState} from 'react'
+import {Container, Content, View, Text, Spinner, Thumbnail} from "native-base"
 import styles from './style';
+import {getItem} from '../../utils/storage';
+import {SECONDARY, USER_INFO} from '../../consts';
 
 export const Profile = () => {
+  const [userInfo, setUserInfo] = useState(null)
+
+  const loadUserInfo = async () => {
+    let userLogged: SetStateAction<any> = await getItem(USER_INFO)
+    userLogged = JSON.parse(userLogged)
+    setUserInfo(userLogged)
+  }
+  useEffect(() => {
+    if(!userInfo) {
+      loadUserInfo()
+    }
+  },[userInfo])
+
+  if(!userInfo) {
+    return (
+      <Spinner color={SECONDARY}/>
+    )
+  }
 
   return (
     <Container>
