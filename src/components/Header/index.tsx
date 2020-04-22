d98df49140acba7da041c9a16e8d0fe926b38b1b
Jeff <jeff.uca@gmail.com>
2020-04-22 11:38:55 +0200

    :recycle: encapsule profile image of user on header component
    before was on home, now only depends of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, {SetStateAction, useEffect, useState} from 'react'
 import {Header as NBHeader, Left, Body, Right, Thumbnail} from 'native-base'
-import { PRIMARY_DARK } from '../../consts';
+import { PRIMARY_DARK, USER_INFO } from '../../consts';
+import { getItem } from '../../utils/storage';
+
 import styles from './style';
 
 export const Header = ({imageUri}: any): JSX.Element => {
+  const [userInfo, setUserInfo] = useState(null)
+
+  const loadUserInfo = async () => {
+    let userLogged: SetStateAction<any> = await getItem(USER_INFO)
+    userLogged = JSON.parse(userLogged)
+    setUserInfo(userLogged)
+  }
+  useEffect(() => {
+    if(!userInfo) {
+      loadUserInfo()
+    }
+  },[userInfo])
+
+
   return (
     <NBHeader androidStatusBarColor={PRIMARY_DARK} style={styles.header}>
       <Left/>
       <Body/>
       <Right>
-        <Thumbnail source={{ uri: imageUri && imageUri }} small/>
+        <Thumbnail source={{ uri: userInfo && userInfo.photoUrl }} small/>
       </Right>
     </NBHeader>
   );
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,28 +1,14 @@
-import React, {useState, useEffect, SetStateAction} from 'react'
+import React  from 'react'
 import {Container, Content, Text, Grid} from 'native-base';
 import { Header } from '../../components/Header'
-import { getItem } from '../../utils/storage';
-import { USER_INFO } from '../../consts';
 import { SearchComponent } from '../../components/SearchComponent';
 import genericStyles from '../../styles'
 
 export const Home = ({navigation}:any): JSX.Element => {
-  const [userInfo, setUserInfo] = useState(null)
-
-  const loadUserInfo = async () => {
-    let userLogged: SetStateAction<any> = await getItem(USER_INFO)
-    userLogged = JSON.parse(userLogged)
-    setUserInfo(userLogged)
-  }
-  useEffect(() => {
-    if(!userInfo) {
-      loadUserInfo()
-    }
-  },[userInfo])
 
   return (
     <Container>
-      <Header imageUri={ userInfo && userInfo.photoUrl }/>
+      <Header />
       <Content style={genericStyles.centeredContent}>
         <Grid style={genericStyles.centeredGrid}>
           <SearchComponent navigation={navigation}/>
