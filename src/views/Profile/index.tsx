06ce930bd8dbe06332881b3c13faffab4fa0891d
Jeff <jeff.uca@gmail.com>
2020-04-22 14:46:38 +0200

    :sparkles: add backbutton from profile

--- src/views/Profile/index.tsx
+++ src/views/Profile/index.tsx
@@ -1,5 +1,5 @@
 import React, {SetStateAction, useEffect, useState} from 'react'
-import {Container, Content, View, Text, Spinner, Thumbnail, Button} from "native-base"
+import {Container, Content, View, Text, Spinner, Thumbnail, Button, Icon} from "native-base"
 import styles from './style';
 import {getItem, clearAll} from '../../utils/storage';
 import {SECONDARY, USER_INFO, LOGIN} from '../../consts';
@@ -23,6 +23,10 @@ export const Profile = ({navigation}:any) => {
     navigation.navigate(LOGIN)
   }
 
+  const handleBackButtonPress = () => {
+    navigation.goBack()
+  }
+
   if(!userInfo) {
     return (
       <Spinner color={SECONDARY}/>
@@ -31,8 +35,16 @@ export const Profile = ({navigation}:any) => {
 
   return (
     <Container>
-      <Content contentContainerStyle={styles.content}>
+      <Content contentContainerStyle={styles.content} >
         <View style={styles.imageContainer} >
+          <Button transparent style={styles.backButton} onPress={handleBackButtonPress}>
+            <Icon
+              android = "md-arrow-back"
+              ios="ios-arrow-back"
+              style={styles.backIcon}
+            />
+          </Button>
+
           <Thumbnail source={{uri: userInfo.photoUrl}} style={styles.profileImage} />
         </View>
         <View style={styles.infoContainer}>
