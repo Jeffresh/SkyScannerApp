68b22fb4d4cca081b8bd41e5c9a7d57933522534
Jeff <jeff.uca@gmail.com>
2020-04-23 11:43:47 +0200

    :wrench: add babel configuration

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,31 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+        [
+          'module-resolver',
+          {
+            root:"./src",
+            extensions: ['.ios.js', 'android.js', '.js', '.ts', '.tsx', '.json'],
+            alias: {
+              "tests": ["./tests/"],
+              "~components": "./src/components",
+              "~Api": "./src/services/api",
+              "~Assets": "./src/assets",
+              "~Components": "./src/components",
+              "~Hooks": "./src/hooks",
+              "~Routes": "./src/routes",
+              "~Services":"./services",
+              "~Screens": "./src/screens",
+              "~Store": "./src/store",
+              "~Styles": "./src/styles",
+              "~Test": "./src/__test__",
+              "~Theme": "./src/theme",
+              "~Utils": "./src/utils"
+            }
+          }
+    ]
+        ]
+
   };
 };
