4ce44dce5a60f59f47f92eebbefc2263d84c0b3a
Jeff <jeff.uca@gmail.com>
2020-04-23 09:44:58 +0200

    :truck: split constants  project and store on different folders

---
+++ src/constants/index.ts
@@ -0,0 +1,24 @@
+// Routes
+export const LOADING_PAGE: string = 'LoadingPage'
+export const LOGIN: string = 'Login'
+export const HOME: string = 'Home'
+export const RESULTS: string = 'Results'
+export const PROFILE: string = 'Profile'
+
+//Fonts
+export const ROBOTO_FONT = {
+  roboto: require('native-base/Fonts/Roboto.ttf'),
+  robotoMedium: require('native-base/Fonts/Roboto_medium.ttf'),
+}
+//Info
+export const ACCESS_TOKEN = '@access-token'
+export const USER_INFO = '@user-info'
+
+//Messages
+export const GOOGLE_SUCCESS_MESSAGE = 'success'
+
+//Colors
+export const PRIMARY = '#512da8'
+export const PRIMARY_DARK = '#311b92'
+export  const SECONDARY = '#f5b72c'
+export const WHITE =   '#fff'
