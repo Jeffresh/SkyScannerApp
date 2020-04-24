f5ce62f6b593e59bd95424572668161ec057a678
Jeff <jeff.uca@gmail.com>
2020-04-24 13:02:15 +0200

    :recycle: :boom: routes has two types of routes to logged/unlogged user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { createStackNavigator } from '@react-navigation/stack'
 import {NavigationContainer} from '@react-navigation/native';
 import {
-  LOADING_PAGE,
   LOGIN,
   HOME,
   RESULTS,
@@ -10,38 +9,50 @@ import {
 } from '~Constants';
 import  Login from '~Views/Login'
 import { Home } from '~Views/Home'
-import LoadingPage from '~Views/LoadingPage'
 import Results from '~Views/Results'
 import { Profile } from '~Views/Profile'
 
-export const RootStack = ():JSX.Element => {
+export const RootStack = ({token}:any):JSX.Element => {
+  const [isLogged, setIsLogged] = useState(token)
   const Stack = createStackNavigator()
+
+  console.log('Route')
+  console.log(isLogged)
+
+  const unLoggedStack = () => {
+    return (
+          <Stack.Screen
+            name={LOGIN}
+            component={Login}
+          />
+          )
+  }
+
+  const loggedStack = () => {
+    return (
+      <>
+      <Stack.Screen
+        name={HOME}
+        component={Home}
+      />
+    <Stack.Screen
+      name={RESULTS}
+      component={Results}
+    />
+    <Stack.Screen
+      name={PROFILE}
+      component={Profile}
+    />
+    </>
+    )
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={LOADING_PAGE}
-        headerMode="none">
-        <Stack.Screen
-          name={LOADING_PAGE}
-          component={LoadingPage}
-        />
-        <Stack.Screen
-          name={LOGIN}
-          component={Login}
-        />
-        <Stack.Screen
-          name={HOME}
-          component={Home}
-        />
-        <Stack.Screen
-          name={RESULTS}
-          component={Results}
-        />
-        <Stack.Screen
-          name={PROFILE}
-          component={Profile}
-        />
+    <NavigationContainer independent={true}>
+      <Stack.Navigator headerMode='none'>
+      {isLogged ? loggedStack(): unLoggedStack()}
       </Stack.Navigator>
-    </NavigationContainer>
+    </ NavigationContainer>
   )
-};
+}
+
