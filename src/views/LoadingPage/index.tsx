aad639f9e101ae54212f953f7f4dad04dd499c27
Jeff <jeff.uca@gmail.com>
2020-04-24 12:57:23 +0200

    :recycle: :boom: loading page now is a component
    no longer a screen

--- src/views/LoadingPage/index.tsx
+++ src/views/LoadingPage/index.tsx
@@ -4,18 +4,7 @@ import {ACCESS_TOKEN, HOME, LOGIN} from '~Constants';
 import { getItem } from '~Utils/storage'
 import styles from './style'
 
-export default ({navigation}:any): JSX.Element => {
-  const redirect = async () => {
-    const token = await getItem(ACCESS_TOKEN)
-    let route = LOGIN
-    if(token){
-      route = HOME
-    }
-    navigation.navigate(route)
-  }
-  useEffect(()=> {
-    redirect()
-  })
+export default (): JSX.Element => {
   return(
     <Container>
       <Content contentContainerStyle={styles.content}>
