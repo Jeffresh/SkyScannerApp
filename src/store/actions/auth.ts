100e2c8c97af0ef172fa12f7bb24de50ae406a49
Jeff <jeff.uca@gmail.com>
2020-04-27 08:20:39 +0200

    :sparkles: add auth actions/create actions

---
+++ src/store/actions/auth.ts
@@ -0,0 +1,23 @@
+import {SIGN_IN, SIGN_OUT, RESTORE_TOKEN} from '~Store/constants/actionTypes';
+
+export interface authPayload {
+  token: string
+}
+
+
+export const signIn = (payload: authPayload) => ({
+    type: SIGN_IN,
+    payload: payload,
+  }
+)
+
+export const signOut = () => ({
+  type: SIGN_OUT
+})
+
+export const restoreToken = (payload: authPayload) => ({
+    type: RESTORE_TOKEN,
+    payload: payload
+
+  }
+)
