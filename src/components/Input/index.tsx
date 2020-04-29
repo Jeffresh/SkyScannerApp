1491793a078c6dfede2cb51bf240389e9072c38a
Jeff <jeff.uca@gmail.com>
2020-04-29 09:18:09 +0200

    :sparkles: add InputWithIcon component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,31 @@
+import {Icon, Input as NativeBaseInput, Item} from "native-base";
+import React from 'react';
+import {NativeSyntheticEvent, StyleProp, TextInputKeyPressEventData, TextStyle} from 'react-native';
+
+interface InputProps {
+  iconName: string,
+  iconIos?: string,
+  iconAndroid?: string,
+  inputPlaceHolder?: string,
+  inputValue: string,
+  onChangeText?: ((text: string) => void) | undefined,
+  onKeyPress?: ((e: NativeSyntheticEvent<TextInputKeyPressEventData>) => void) | undefined,
+  InputStyle?: StyleProp<TextStyle>,
+  IconStyle ?: StyleProp<TextStyle>,
+
+}
+
+export const Input: React.FC<InputProps> = (props:InputProps) => {
+  return (
+    <>
+      <Icon name={props.iconName} ios={props.iconIos} android={props.iconAndroid} style={props.IconStyle}/>
+      <NativeBaseInput
+        placeholder={props.inputPlaceHolder}
+        value={props.inputValue}
+        onChangeText={props.onChangeText}
+        onKeyPress={props.onKeyPress}
+        style={props.InputStyle}
+      />
+    </>
+  )
+}
