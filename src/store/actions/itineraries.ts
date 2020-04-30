b055eb0201c15dc635d149980e7daab42a714800
Jeff <jeff.uca@gmail.com>
2020-04-30 11:37:24 +0200

    :sparkles: add action to delete places

--- src/store/actions/itineraries.ts
+++ src/store/actions/itineraries.ts
@@ -1,13 +1,17 @@
-import { GET_LOCATIONS_START, GET_ITINERARIES_START } from '~Store/constants/actionTypes';
+import { GET_LOCATIONS_START, DEL_LOCATIONS_SUCCESS, GET_ITINERARIES_START } from '~Store/constants/actionTypes';
 
 export interface getLocationPayload {
-    query: string
+  query: string,
 }
 
-export const getLocations = (payload: getLocationPayload) => (
-  {
+export const getLocations = (payload: getLocationPayload) => ({
     type: GET_LOCATIONS_START,
     payload: payload
+  }
+)
+
+export const delLocations = () => ({
+    type: DEL_LOCATIONS_SUCCESS
   }
 )
 
